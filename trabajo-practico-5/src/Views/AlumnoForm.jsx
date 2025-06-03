66affa9171d64b122c03a23e6d775dc905b71ea4
Facundo897 <facundogaspar897@gmail.com>
2025-06-03 11:28:03 -0300

    feat: se agrega componente funcional AlumnoForm

---
+++ trabajo-practico-5/src/Views/AlumnoForm.jsx
@@ -0,0 +1,40 @@
+import { useNavigate } from "react-router-dom";
+import Titulo from "./Titulo";
+
+function AlumnoForm({alumno, handleSubmit}) {
+  const [alumnoEdit, setAlumnoEdit] = alumno;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Titulo texto={"Registro de Alumnos"}></Titulo>
+      <form actiion="" onSubmit={(e) =>{
+        e.preventDefault();
+        handleSubmit();
+        navigate("");
+      }}>
+        <label>LU:</label>
+        <input id="LU" type="text" value={alumnoEdit.lu || ""} onChange={(e) => setAlumnoEdit({...alumnoEdit, lu:e.target.value})} required></input>
+        <b></b>
+        <label>Nombre del alumno:</label>
+        <input id="nombre" type="text" value={alumnoEdit.nombre || ""} onChange={(e) => setAlumnoEdit({...alumnoEdit, nombre:e.target.value})} required></input>
+        <b></b>
+        <label>Apellido del alumno:</label>
+        <input id="apellido" type="text" value={alumnoEdit.apellido || ""} onChange={(e) => setAlumnoEdit({...alumnoEdit, apellido:e.target.value})} required></input>
+        <b></b>
+        <label>Email:</label>
+        <input id="email" type="email" value={alumnoEdit.email || ""} onChange={(e) => setAlumnoEdit({...alumnoEdit, email:e.target.value})} required></input>
+        <b></b>
+        <label>Domicilio:</label>
+        <input id="domicilio" type="text" value={alumnoEdit.domicilio || ""} onChange={(e) => setAlumnoEdit({...alumnoEdit, domicilio:e.target.value})} required></input>
+        <b></b>
+        <label>Telefono:</label>
+        <input id="telefono" type="tel" value={alumnoEdit.telefono || ""} onChange={(e) => setAlumnoEdit({...alumnoEdit, telefono:e.target.value})} required></input>
+        <b></b>
+        <button>Guardar</button>
+      </form>
+    </div>
+  );
+}
+
+export default AlumnoForm;
---
+++ trabajo-practico-5/src/Views/Titulo.jsx
@@ -0,0 +1,6 @@
+function Titulo({texto}){
+    return(
+        <h3>{texto}</h3>
+    );
+}
+export default Titulo;
