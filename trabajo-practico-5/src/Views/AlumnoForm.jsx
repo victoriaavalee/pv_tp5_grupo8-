9292cafc412abe63cc97976b5a03d998ea513a80
Facundo897 <facundogaspar897@gmail.com>
2025-06-03 12:43:13 -0300

    fix: correcion de etiquetas html  de AlumnoForm

--- trabajo-practico-5/src/Views/AlumnoForm.jsx
+++ trabajo-practico-5/src/Views/AlumnoForm.jsx
@@ -8,29 +8,29 @@ function AlumnoForm({alumno, handleSubmit}) {
   return (
     <div>
       <Titulo texto={"Registro de Alumnos"}></Titulo>
-      <form actiion="" onSubmit={(e) =>{
+      <form action="" onSubmit={(e) =>{
         e.preventDefault();
         handleSubmit();
         navigate("");
       }}>
         <label>LU:</label>
         <input id="LU" type="text" value={alumnoEdit.lu || ""} onChange={(e) => setAlumnoEdit({...alumnoEdit, lu:e.target.value})} required></input>
-        <b></b>
+        <br></br>
         <label>Nombre del alumno:</label>
         <input id="nombre" type="text" value={alumnoEdit.nombre || ""} onChange={(e) => setAlumnoEdit({...alumnoEdit, nombre:e.target.value})} required></input>
-        <b></b>
+        <br></br>
         <label>Apellido del alumno:</label>
         <input id="apellido" type="text" value={alumnoEdit.apellido || ""} onChange={(e) => setAlumnoEdit({...alumnoEdit, apellido:e.target.value})} required></input>
-        <b></b>
+        <br></br>
         <label>Email:</label>
         <input id="email" type="email" value={alumnoEdit.email || ""} onChange={(e) => setAlumnoEdit({...alumnoEdit, email:e.target.value})} required></input>
-        <b></b>
+        <br></br>
         <label>Domicilio:</label>
         <input id="domicilio" type="text" value={alumnoEdit.domicilio || ""} onChange={(e) => setAlumnoEdit({...alumnoEdit, domicilio:e.target.value})} required></input>
-        <b></b>
+        <br></br>
         <label>Telefono:</label>
         <input id="telefono" type="tel" value={alumnoEdit.telefono || ""} onChange={(e) => setAlumnoEdit({...alumnoEdit, telefono:e.target.value})} required></input>
-        <b></b>
+        <br></br>
         <button>Guardar</button>
       </form>
     </div>
