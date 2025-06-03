3110aa9d019da0c68e24c3af54a09a52aa96541a
Facundo897 <facundogaspar897@gmail.com>
2025-06-03 11:30:52 -0300

    feat: agregado de la funcion guardar y editar alumnos

--- trabajo-practico-5/src/App.jsx
+++ trabajo-practico-5/src/App.jsx
@@ -1,35 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+let nextId = 0;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [alumnos, setAlumnos] = useState ([
+    { id: 100 ,lu: "APU00999", nombre: "Marta Eugenia", apellido: "Diaz", curso: "Tercero" ,email: "marianadiaz@mail.com", domicilio: "Av. Congreso 125", telefono: "3884895999"},
+    { id: 101, lu: "APU01005", nombre: "Luciano Valentín", apellido: "Ramirez", curso: "Segundo", email: "lucianovramirez@mail.com", domicilio: "Calle Lavalle 145", telefono: "3885111222"},
+    { id: 102, lu: "APU01006", nombre: "Camila Soledad", apellido: "Torres", curso: "Primero", email: "camilastorres@mail.com", domicilio: "Av. El Éxodo 201", telefono: "3884777888"},
+    { id: 103, lu: "APU01007", nombre: "Joaquín Andrés", apellido: "López", curso: "Tercero", email: "joaquinandreslopez@mail.com", domicilio: "Alvear 300", telefono: "3884000222"},
+    { id: 104, lu: "APU01008", nombre: "Valentina Pilar", apellido: "Moreno", curso: "Segundo", email: "valenpmoreno@mail.com", domicilio: "Av. Córdoba 645", telefono: "3884333777"},
+    { id: 105, lu: "APU01009", nombre: "Franco Emmanuel", apellido: "Gómez", curso: "Primero", email: "francoegomez@mail.com", domicilio: "España 550", telefono: "3884555999"}
+  ]);
+  const [alumno, setAlumno] = useState({});
+
+
+  //Guardar y editar alumnos
+  const handleSubmit = (e) =>{
+    if (typeof alumno.id === "undefined"){
+      alumno.id = nextId++;
+      setAlumnos([...alumnos,alumno]);
+    }else{
+      setAlumnos(alumnos => [...alumnos.filter(u =>u.id !== alumno.id), alumno]);
+    }
+    setAlumno({});
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
