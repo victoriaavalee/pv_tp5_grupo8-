662056d2993f0115b9086815ec03ec46def1080c
victoriaavalencia <victoriavale754@gmail.com>
2025-06-04 20:34:30 -0300

    avance listado alumno

--- trabajo-practico-5/src/Views/listadoAlumnos.jsx
+++ trabajo-practico-5/src/Views/listadoAlumnos.jsx
@@ -1,1 +1,44 @@
 import { useNavigate } from "react-router-dom";
+import Titulo from "./Titulo";
+
+
+const ListaAlumnos = ({ alumnos }) => {
+    <Titulo texto={"Listado de Alumnos"}></Titulo>
+    return(
+        <table className="w-full border">
+            <thead>
+                <tr>
+                    <th >LU</th>
+                    <th >Nombre</th>
+                    <th >Apellido</th>
+                    <th >Curso</th>
+                    <th >Acciones</th>
+                    <th >Email</th>
+                    <th >Domicilio</th>
+                    <th >Telefono</th>
+                </tr>
+            </thead>
+            <tbody>
+                {Array.isArray(alumnos) && alumnos.length > 0 ?(
+                    alumnos.map((alumno)=> (
+                        <tr key={alumno.lu}>
+                            <td >{alumno.lu}</td>
+                            <td >{alumno.nombre}</td>
+                            <td >{alumno.apellido}</td>
+                            <td >{alumno.curso}</td>
+                            <td >{alumno.email}</td>
+                            <td >{alumno.domicilio}</td>
+                            <td >{alumno.telefono}</td>
+                        </tr>
+                    ))
+                ):(
+                    <tr>
+                        <td colSpan="7">No hay alumnos disponibles</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    )
+}
+
+export default ListaAlumnos;
